53afea7344032a30c65b394c2bf2f0d794702611
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-05-22 22:05:40 -0300

    feat: adding new book routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,7 +3,10 @@ import { bookController } from '../controllers/bookController.js';
 
 const router = express.Router();
 
-router.get('/books', bookController.listAllBooks);
+router.get('/books', bookController.findAllBooks);
+router.get('/books/:id', bookController.findOneBookById);
 router.post('/books', bookController.createBook);
+router.put('/books/:id', bookController.updateBook);
+router.delete('/books/:id', bookController.deleteBook);
 
 export default router;
